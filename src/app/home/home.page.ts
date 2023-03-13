2dc8eb5ee8afe2e434618a83b901e4fcdcff32d3
pHerediaCriollo <paulheredia106@gmail.com>
2023-03-13 08:43:07 -0500

    Envio de datos

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { title } from 'process';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  tasks:(title:String;completed:boolean;)[]=
+  [
+    (title:"Tender la cama", completed:false),
+    (title:"Lavar los platos", completed:false),
+    (title:"Hacer deberes", completed:false)
+  ]
+  newTask = ' ';
 
-  constructor() {}
+  addTask(){
+    if(this.newTask.trim().length===0){
+      return;
+    }
+    this.tasks.push({title:this.newTask,completed:false})
+    this.newTask=' ';
+  }
 
+  deleteTask(task:{title:String;completed:boolean}){
+    const index = this.tasks.indexOf(task);
+    this.tasks.splice(index,1);
+  }
+
+  getProgress(){
+    const totalTasks =this.tasks.length;
+    const completedTasks = this.tasks.filter(task => task.completed).length;
+
+  }
 }
